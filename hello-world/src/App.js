7cda69b610e32aec112cf890221d04a19c6a597d
Filipe Vilela <fvf0092000@gmail.com>
2023-05-06 11:08:24 -0300

    event handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,26 +4,27 @@ import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
-import Counter from './components/Counter'
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Diana" heroName="Wonder Woman" />
-      <Welcome name="Bruce" heroName="Batman"></Welcome>
+      <FunctionClick />
+      <ClassClick  />
+      {/*<Counter />*/}
+      {/*<Message />*/}
       {/*<Greet name="Bruce" heroName="Batman">
-        <p>This is children prop</p>
-      </Greet>
-      <Greet name="Clark" heroName="Superman">
+          <p>This is children prop</p>
+      </Greet>*/}
+      {/*<Greet name="Clark" heroName="Superman">
         <button>Action</button>
-      </Greet>
-      <Counter />
-      <Message />
-      <Welcome name="Bruce" heroName="Batman">
-      </Welcome>
-      <Welcome name="Clark" heroName="Superman">
-      </Welcome>
-      <Hello/>*/}
+      </Greet>*/}
+      {/*<Greet name="Diana" heroName="Wonder Woman" />*/}
+      {/*<Welcome name="Bruce" heroName="Batman" />*/}
+      {/*<Welcome name="Clark" heroName="Superman">*/}
+      {/*<Hello/>*/}
     </div>
   );
 }
