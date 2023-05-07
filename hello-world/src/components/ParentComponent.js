db0ae90e5e73531f8ac0e300319bb2d5dd21a08d
Filipe Vilela <fvf0092000@gmail.com>
2023-05-07 00:30:20 -0300

    methods as props

---
+++ hello-world/src/components/ParentComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import ChildComponent from './ChildComponent'
+
+class ParentComponent extends React.Component {
+    
+    constructor() {
+        super()
+        this.state = {
+            parentName: 'parent'
+        }
+        this.greetParent = this.greetParent.bind(this)
+    }
+
+    greetParent(childName) {
+        alert(`Hello ${this.state.parentName} from ${childName}`) // Este é um recurso do es6 que substitui a concatenação
+    }
+    
+    render() {
+        return(
+            <div>
+                <ChildComponent greetHandler={this.greetParent} />
+            </div>
+        )
+    }
+}
+
+export default ParentComponent
